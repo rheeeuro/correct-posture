1664e4260e959970fd941068f2468e7e77ad56a3
eurohand <eurohand@naver.com>
2020-06-15 03:32:32 +0900

    static path

--- src/app.js
+++ src/app.js
@@ -23,7 +23,7 @@ const CokieStore = MongoStore(session);
 app.use(helmet());
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
-app.use("/static", express.static("static"));
+app.use("/static", express.static(path.join(__dirname, "static")));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
